d4532be693c549f08b33d8864b27bb08151d184d
rosa <rosatiaragaluh@gmail.com>
2021-12-19 14:20:48 +0700

    designing TasksView

--- Shared/Views/TasksView.swift
+++ Shared/Views/TasksView.swift
@@ -5,4 +5,16 @@
 //  Created by T on 19/12/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct TasksView: View{
+    var body: some View {
+        Text("fjsdkfsdfkd")
+    }
+}
+
+struct TasksView_Previews: PreviewProvider {
+    static var previews: some View {
+        TasksView()
+    }
+}
