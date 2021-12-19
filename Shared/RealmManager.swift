4ea53ba4489903816cbae561b97f8811742d220d
rosa <rosatiaragaluh@gmail.com>
2021-12-19 15:36:06 +0700

    create init function for Realm

--- Shared/RealmManager.swift
+++ Shared/RealmManager.swift
@@ -11,7 +11,22 @@ import RealmSwift
 // allow the entire app or all the views connected to the RealmManager
 // to be notified of changes in this class
 class RealmManager: ObservableObject {
-    
-    // priavte(set) means we can only set the variable between the RealmManager class.
+    // private(set) means we can only set the variable between the RealmManager class.
+    // '?' means optional because opening a Realm might throw an error.
     private(set) var localRealm: Realm?
+    //open Realm everytime we initialize the class.
+    init(){
+        openRealm()
+    }
+    func openRealm(){
+        do {
+            let config = Realm.Configuration(schemaVersion: 1)
+            Realm.Configuration.defaultConfiguration = config
+            
+            // create a Realm
+            localRealm = try Realm()
+        } catch {
+            print("Error opening Realm \(error)")
+        }
+    }
 }
