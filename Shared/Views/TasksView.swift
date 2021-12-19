c5e9d313ffd73fe32f607a58aa3bedb3c3d6cd88
rosa <rosatiaragaluh@gmail.com>
2021-12-19 17:16:37 +0700

    all developments are finished.

--- Shared/Views/TasksView.swift
+++ Shared/Views/TasksView.swift
@@ -13,22 +13,24 @@ struct TasksView: View{
         VStack {
             Text("My tasks")
                 .foregroundColor(Color(red: 185 / 255, green: 117 / 255, blue: 80 / 255))
-                .font(.title2)
-                .bold()
+                .font(.title2).bold()
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding()
+                .padding(.top, 30)
             List {
                 ForEach(realmManager.tasks, id: \.id){
                     task in
-                    TaskRow(task: task.title, completed: task.completed)
-                        .onTapGesture {
-                            realmManager.updateTask(id: task.id, completed:!task.completed)
-                        }
-                        .swipeActions(edge: .trailing){
-                            Button(role: .destructive){
-                                realmManager.deleteTask(id: task.id)
-                            } label: {
-                                 Image("trash")
+                    if !task.isInvalidated {
+                        TaskRow(task: task.title, completed: task.completed)
+                            .onTapGesture {
+                                realmManager.updateTask(id: task.id, completed:!task.completed)
+                            }
+                            .swipeActions(edge: .trailing){
+                                Button(role: .destructive){
+                                    realmManager.deleteTask(id: task.id)
+                                } label: {
+                                     Image("trash")
+                            }
                         }
                     }
                 }
