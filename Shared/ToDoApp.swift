6ee6ae89cfc6bb0c5871e70da16fc007f9d65662
rosa <rosatiaragaluh@gmail.com>
2021-12-19 13:40:46 +0700

    Initial Commit

---
+++ Shared/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  Shared
+//
+//  Created by T on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
