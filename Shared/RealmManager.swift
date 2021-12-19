a6e89045a1210a0ddffd1a6245e531a387525b8d
rosa <rosatiaragaluh@gmail.com>
2021-12-19 15:31:38 +0700

    add RealmManager file

---
+++ Shared/RealmManager.swift
@@ -0,0 +1,15 @@
+//
+//  RealmManager.swift
+//  ToDo
+//
+//  Created by T on 19/12/21.
+//
+
+import Foundation
+import RealmSwift
+
+// allow the entire app or all the views connected to the RealmManager
+// to be notified of changes in this class
+class RealmManager: ObservableObject {
+    
+}
