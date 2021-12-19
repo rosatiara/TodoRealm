67db9c6ec5beaf345a70efcf0d4ee0cbd4037643
rosa <rosatiaragaluh@gmail.com>
2021-12-19 14:52:52 +0700

    finished designing AddTaskView

--- Shared/Views/AddTaskView.swift
+++ Shared/Views/AddTaskView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
-
 struct AddTaskView: View {
+    @State private var title: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20){
+            Text("Create a new task")
+                .foregroundColor(Color(red: 185 / 255, green: 117 / 255, blue: 80 / 255))
+                .font(.title2).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            TextField("Enter your task", text: $title)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                print("Task added!")
+            } label: {
+                Text("Add task")
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .cornerRadius(30)
+                    .background(Color(red: 203 / 255, green: 153 / 255, blue: 126 / 255)).ignoresSafeArea()
+            }
+            Spacer()
+        }.padding(.top,70)
+        .padding(.horizontal)
+        .background(Color(red: 237 / 255, green: 220 / 255, blue: 210 / 255)).ignoresSafeArea()
     }
 }
 
