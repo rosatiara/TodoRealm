4bb5bdb05b81a4c162b530c056fd180ca7cb496b
rosa <rosatiaragaluh@gmail.com>
2021-12-19 15:40:29 +0700

    add Task class

---
+++ Shared/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  ToDo
+//
+//  Created by T on 19/12/21.
+//
+
+import Foundation
+import RealmSwift
+
+// Realm access classes as Model so we need to create a class
+// Object & ObjectKeyIndentifiable are protocols from Realm.
+class Task: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title = ""
+    @Persisted var completed = false
+}
