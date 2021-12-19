8b53d3566b66582ca2ee71c0ee5a7b509dc85de3
rosa <rosatiaragaluh@gmail.com>
2021-12-19 14:24:14 +0700

    add TasksView bg color

--- Shared/Views/TasksView.swift
+++ Shared/Views/TasksView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct TasksView: View{
     var body: some View {
-        Text("fjsdkfsdfkd")
+        VStack {
+            Text("hi")
+        }
+        // make the VStack takes the entire screen
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 0 / 255, green: 8 / 255, blue: 20 / 255))
     }
 }
 
